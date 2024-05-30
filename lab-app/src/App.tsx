f7d5f99ae8169d7fbe9a789317fc556947e5c2a8
ash <165500454+ashwanikumar23@users.noreply.github.com>
2024-05-30 04:18:24 +0000

    sign in

--- lab-app/src/App.tsx
+++ lab-app/src/App.tsx
@@ -4,12 +4,11 @@ import DashBoard from './Components/Layout/Dashboard';
 import PdfView from './Components/Layout/PDF/PdfView';
 import PdfLayout from './Components/PDF/PdfLayout';
 function App() {
-  const [uniqueId, setUniqueId] = React.useState("");
+  const [sign, setSign] = React.useState(false);
+  const getToken=()=>{
+    
+  }
   useEffect(() => {
-    // Generate a unique ID when the component mounts
-    const id = "id-" + Math.random().toString(36)
-    console.log("mm",id);
-    setUniqueId(id);
   }, []); 
   return (
     <>
