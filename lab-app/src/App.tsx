45cdc0093cf2a3a493e54b160a3efa4abcd89b48
ash <165500454+ashwanikumar23@users.noreply.github.com>
2024-05-30 05:15:35 +0000

    update sign in UI

--- lab-app/src/App.tsx
+++ lab-app/src/App.tsx
@@ -3,16 +3,18 @@ import React, { useEffect } from 'react';
 import DashBoard from './Components/Layout/Dashboard';
 import PdfView from './Components/Layout/PDF/PdfView';
 import PdfLayout from './Components/PDF/PdfLayout';
+import SignIn from './Pages/sign-in';
 function App() {
   const [sign, setSign] = React.useState(false);
   const getToken=()=>{
-    
+
   }
   useEffect(() => {
   }, []); 
   return (
     <>
-    <DashBoard />
+    <SignIn />
+    {/* <DashBoard /> */}
     {/* <PdfLayout /> */}
     </>
   );
