0be9d583f04dcb0bd60fd170d32332648033b5a0
ash <165500454+ashwanikumar23@users.noreply.github.com>
2024-06-05 07:08:15 +0000

    side manu update

--- lab-app/src/shared/rightSideCard/rightSideCard.tsx
+++ lab-app/src/shared/rightSideCard/rightSideCard.tsx
@@ -10,50 +10,50 @@ const items: MenuItem[] = [
   {
     key: '1',
     icon: <MailOutlined />,
-    label: 'Navigation One',
+    label: 'Lab settings',
     children: [
-      { key: '11', label: 'Option 1' },
-      { key: '12', label: 'Option 2' },
-      { key: '13', label: 'Option 3' },
-      { key: '14', label: 'Option 4' },
+      { key: '11', label: 'Settings' },
+      { key: '12', label: 'PDF Setting' },
+      { key: '12', label: 'Pending' },
+
     ],
   },
-  {
-    key: '2',
-    icon: <AppstoreOutlined />,
-    label: 'Navigation Two',
-    children: [
-      { key: '21', label: 'Option 1' },
-      { key: '22', label: 'Option 2' },
-      {
-        key: '23',
-        label: 'Submenu',
-        children: [
-          { key: '231', label: 'Option 1' },
-          { key: '232', label: 'Option 2' },
-          { key: '233', label: 'Option 3' },
-        ],
-      },
-      {
-        key: '24',
-        label: 'Submenu 2',
-        children: [
-          { key: '241', label: 'Option 1' },
-          { key: '242', label: 'Option 2' },
-          { key: '243', label: 'Option 3' },
-        ],
-      },
-    ],
-  },
+  // {
+  //   key: '2',
+  //   icon: <AppstoreOutlined />,
+  //   label: 'Navigation Two',
+  //   children: [
+  //     { key: '21', label: 'Option 1' },
+  //     { key: '22', label: 'Option 2' },
+  //     {
+  //       key: '23',
+  //       label: 'Submenu',
+  //       children: [
+  //         { key: '231', label: 'Option 1' },
+  //         { key: '232', label: 'Option 2' },
+  //         { key: '233', label: 'Option 3' },
+  //       ],
+  //     },
+  //     {
+  //       key: '24',
+  //       label: 'Submenu 2',
+  //       children: [
+  //         { key: '241', label: 'Option 1' },
+  //         { key: '242', label: 'Option 2' },
+  //         { key: '243', label: 'Option 3' },
+  //       ],
+  //     },
+  //   ],
+  // },
   {
     key: '3',
     icon: <SettingOutlined />,
-    label: 'Navigation Three',
+    label: 'General settings',
     children: [
-      { key: '31', label: 'Option 1' },
+      { key: '31', label: 'Upload Logo' },
       { key: '32', label: 'Option 2' },
       { key: '33', label: 'Option 3' },
-      { key: '34', label: 'Option 4' },
+      { key: '34', label: 'Copy rights' },
     ],
   },
 ];
